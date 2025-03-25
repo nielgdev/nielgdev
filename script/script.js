6d9f97b16a77ea89ba6c6cdac948cc4307e33e8d
Niel GDEV <110709264+nielgdev@users.noreply.github.com>
2025-03-25 22:36:46 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,56 @@
+const resumeBtns = document.querySelectorAll('.resume-btn');
+
+resumeBtns.forEach((btn, idx) => 
+{
+	btn.addEventListener('click', () => 
+	{
+		const resumeDetails = document.querySelectorAll('.resume-detail');
+		resumeBtns.forEach(btn => 
+		{
+			btn.classList.remove('active');
+		});
+		btn.classList.add('active');
+
+		resumeDetails.forEach(detail => 
+		{
+			detail.classList.remove('active');
+		});
+		resumeDetails[idx].classList.add('active');
+	});
+});
+
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+
+const activePortfolio = () =>
+{
+	const imgSlide = document.querySelector('.portfolio-carousel .img-slide');
+	imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+}
+arrowRight.addEventListener('click', () =>
+{
+	if (index < 4)
+	{
+		index++;
+	}
+	else
+	{
+		index = 5;
+	}
+	activePortfolio();
+});
+
+arrowLeft.addEventListener('click', () =>
+{
+	if (index > 1)
+	{
+		index--;
+	}
+	else
+	{
+		index = 0;
+	}
+	activePortfolio();
+});
